6f099329d5c663ef408daee38db680c4c4694d8b
sinmahod <sinmahod@qq.com>
2017-02-18 00:36:24 +0800

    update

--- conf/dbconn.go
+++ conf/dbconn.go
@@ -1,12 +1,37 @@
 package conf
 
 import (
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
+	"strings"
+)
+
+var (
+	DBType     string
+	DBLocal    string
+	DBPort     string
+	DBName     string
+	DBUserName string
+	DBPassword string
 )
 
 func RegisterDB() {
-	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "root:qweqwe@tcp(localhost:3306)/beestudy?charset=utf8") //60.205.164.3
-	orm.Debug = true
+	DBType = strings.ToLower(beego.AppConfig.String("dbtype"))
+	DBLocal = beego.AppConfig.String("dblocal")
+	DBPort = beego.AppConfig.String("dbport")
+	DBName = beego.AppConfig.String("dbname")
+	DBUserName = beego.AppConfig.String("dbusername")
+	DBPassword = beego.AppConfig.String("dbpassword")
+
+	orm.RegisterDriver(DBType, orm.DRMySQL)
+	if DBType == "mysql" {
+		orm.RegisterDataBase("default", DBType, DBUserName+":"+DBPassword+"@tcp("+DBLocal+":"+DBPort+")/"+DBName+"?charset=utf8") //60.205.164.3
+	}
+	mode := beego.AppConfig.String("runmode")
+
+	if mode == "dev" {
+		orm.Debug = true
+	}
+
 }
